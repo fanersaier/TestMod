ee244cd0093c57ac6cd9893852ea9161f73bf0b5
fanersaier <1330794157@qq.com>
2024-12-13 13:54:30 +0800

    1

---
+++ src/main/java/zyq/test_mod/mixin/TestModClient.java
@@ -0,0 +1,9 @@
+package zyq.test_mod.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
